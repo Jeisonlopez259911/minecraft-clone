30eb9b5db152f75a256f882760ea15e0dec448c0
Jeisonlopez259911 <jeisonlopez2599@gmail.com>
2023-04-25 15:50:32 -0500

    Se hizo un clon de minecraft con react.js three.js y mas

---
+++ src/components/Cube.jsx
@@ -0,0 +1,35 @@
+import { useStore } from "../hooks/useStore"
+import { useBox } from "@react-three/cannon"
+import * as textures from '../images/textures'
+import { useState } from "react"
+
+
+export const Cube = ({id, position, texture}) => {
+    const [isHovered, setIsHovered] = useState(false)
+    const [removeCube] = useStore(state => [state.removeCube])
+
+    const [ref] = useBox(() => ({
+        type: 'Static',
+        position
+    }))
+
+    const activeTexture = textures[texture + 'Texture']
+
+    return (
+        <mesh 
+            ref={ref} 
+            onPointerMove={(e) => { e.stopPropagation(); setIsHovered(true);}} 
+            onPointerOut={(e) => { e.stopPropagation(); setIsHovered(false);}}
+            onClick={(e) => {
+                e.stopPropagation();
+                if (e.altKey) {
+                    removeCube(id)
+                }
+
+            }}
+        >
+            <boxBufferGeometry attach="geometry" />
+            <meshStandardMaterial  color={isHovered ? 'grey' : 'white'} transparent map={activeTexture} attach="material"/>
+        </mesh>
+    )
+}
